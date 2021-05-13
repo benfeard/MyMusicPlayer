4aa48befd7e933c39a7c606758b3fbd39761723b
Benfeard <benfeard@gmail.com>
2021-05-12 22:18:33 -0400

    Initial Commit

---
+++ Shared/MyMusicPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMusicPlayerApp.swift
+//  Shared
+//
+//  Created by Benfeard Williams on 5/12/21.
+//
+
+import SwiftUI
+
+@main
+struct MyMusicPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
